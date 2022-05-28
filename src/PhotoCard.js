289bab89a9b6133fcace97dd4e48af23392f5efa
Ash <smashraful1995@gmail.com>
2022-05-28 16:26:12 +0600

    1st commit

---
+++ src/PhotoCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+
+function PhotoCard() {
+  return (
+    <>
+      <article>
+        <Avatar
+          alt="Ashraful"
+          src="https://scontent.fjsr3-1.fna.fbcdn.net/v/t39.30808-6/273008205_1816387028545284_8493737343261028645_n.jpg?stp=c0.169.1536.1536a_dst-jpg_s526x395&_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHQeAp1HXpdBQOU5KUFXfTAufYlHf3LhKK59iUd_cuEoo71sqM6RESjiiRY1fVzkgHY7yNSeTj-yffzVCQwKYyy&_nc_ohc=pFcO57a5YCwAX_3XY5W&_nc_oc=AQmoQjwcA93qtl2770-AadcNQdLb5cEF2enDgE8URJaybPHjpcFXw3QWmJ71Yhu49ps&_nc_ht=scontent.fjsr3-1.fna&oh=00_AT9SY_g8YXKAFB-7YyNezNI8WgQe7aK1--HaGHv8SVQNAw&oe=6249D3B5"
+        />
+        <h3>Sm Ashraful Islam</h3>
+        <p>
+          <h5>Total Bill: 1200 TK</h5>
+        </p>
+        <h5>Asset: 100 TK</h5>
+      </article>
+      {/* <div className="container"></div> */}
+      {/* <div className="container">
+        <Avatar
+          alt="Mehdi"
+          src="https://scontent.fjsr3-1.fna.fbcdn.net/v/t1.6435-9/139753565_1816666341816539_7975088046821006572_n.jpg?stp=dst-jpg_p640x640&_nc_cat=103&ccb=1-5&_nc_sid=e3f864&_nc_eui2=AeFEMqFbKoJRH9ih4Vszi50cuUh9gg5L4bi5SH2CDkvhuEjxg5w35j9qqBVa_tXvuDPFKZ8KmYtJidQIsSuHfGT5&_nc_ohc=qKKm4RS1iNsAX_tzZtL&_nc_ht=scontent.fjsr3-1.fna&oh=00_AT9SvjilOU_7vA5g0xb_VhP7CldWogB1TssJeMK2F5bCtw&oe=62679667"
+        />
+        <h3>Mehedi Hasan Rabbi</h3>
+        <p>
+          <h5>Total Bill: 1200 TK</h5>
+        </p>
+        <h5>Asset: 100 TK</h5>
+      </div>
+      <div className="container">
+        <Avatar
+          alt="Jwel"
+          src="https://scontent.fjsr3-1.fna.fbcdn.net/v/t1.6435-9/115805372_919431631890482_7387748075136554249_n.jpg?stp=dst-jpg_s960x960&_nc_cat=102&ccb=1-5&_nc_sid=e3f864&_nc_eui2=AeGrhbCItdS7xvNMby-8wnw8_Jm53f0h2mD8mbnd_SHaYN1C45iju6H_AaWTDGVBaIRtaG9vQMB8-Dfk0ZVNkorV&_nc_ohc=5__EzNJZynAAX_Jbnpb&_nc_ht=scontent.fjsr3-1.fna&oh=00_AT-nvP3cQAMef9rgOzqxXxYlSpnA4QVtcCh56T-Nil8aOg&oe=626B4C1C"
+        />
+        <h3>Md Jwuel</h3>
+        <p>
+          <h5>Total Bill: 1200 TK</h5>
+        </p>
+        <h5>Asset: 100 TK</h5>
+      </div> */}
+    </>
+  );
+}
+
+export default PhotoCard;
